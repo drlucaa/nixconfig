df5f25db7c19ec1aa1a1b18ff8afe8b743936d2e
Luca Fondo <luca.fondo@trai.ch>
2025-10-31 04:26:13 +0100

    Refactor

--- modules/programs/zoxide.nix
+++ modules/programs/zoxide.nix
@@ -4,20 +4,19 @@
   config,
   ...
 }:
-with lib;
 let
   cfg = config.modules.programs.zoxide;
 in
 {
   options.modules.programs.zoxide = {
-    enable = mkOption {
-      type = types.bool;
+    enable = lib.mkOption {
+      type = lib.types.bool;
       default = true;
       description = "Enable zoxide";
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home-manager.users.${username} =
       { pkgs, ... }:
       {
