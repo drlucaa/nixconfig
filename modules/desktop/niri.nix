456bf42534c0721da43baf4f51a58b55c1baa09c
Luca Fondo <luca.fondo@trai.ch>
2025-11-02 15:07:52 +0100

    Remove custom shell command workaround

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -88,83 +88,83 @@
           gestures.hot-corners.enable = false;
 
           # Minimal keybindings for basic usability
-          binds =
-            with config.lib.niri.actions;
-            let
-              sh = spawn "sh" "-c";
-            in
-            {
-              # Essential actions
-              "Mod+Q".action = close-window;
-              "Mod+T".action = spawn "ghostty";
-              "Mod+B".action = spawn "firefox";
-              "Mod+Space".action = spawn "walker";
-              "Mod+W".action = sh "swww img $(fd --full-path ~/Pictures/Wallpapers --type symlink | shuf -n 1)";
+          binds = with config.lib.niri.actions; {
+            # Essential actions
+            "Mod+Q".action = close-window;
+            "Mod+T".action = spawn "ghostty";
+            "Mod+B".action = spawn "firefox";
+            "Mod+Space".action = spawn "walker";
+            "Mod+W".action =
+              spawn-sh "swww img $(fd --full-path ~/Pictures/Wallpapers --type symlink | shuf -n 1)";
 
-              # Focus movement
-              "Mod+Shift+H".action = focus-monitor-left;
-              "Mod+Shift+J".action = focus-monitor-down;
-              "Mod+Shift+K".action = focus-monitor-up;
-              "Mod+Shift+L".action = focus-monitor-right;
-              "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
-              "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
-              "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
-              "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
+            # Focus movement
+            "Mod+Shift+H".action = focus-monitor-left;
+            "Mod+Shift+J".action = focus-monitor-down;
+            "Mod+Shift+K".action = focus-monitor-up;
+            "Mod+Shift+L".action = focus-monitor-right;
+            "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
+            "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
+            "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
+            "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
 
-              # Window movement
-              "Mod+H".action = focus-column-left;
-              "Mod+J".action = focus-window-down;
-              "Mod+K".action = focus-window-up;
-              "Mod+L".action = focus-column-right;
-              "Mod+Ctrl+H".action = move-column-left;
-              "Mod+Ctrl+J".action = move-window-down;
-              "Mod+Ctrl+K".action = move-window-up;
-              "Mod+Ctrl+L".action = move-column-right;
-              "Mod+Home".action = focus-column-first;
-              # "Mod+End ".action = focus-column-last;
-              "Mod+Ctrl+Home".action = move-column-to-first;
-              # "Mod+Ctrl+End ".action = move-column-to-last;
+            # Window movement
+            "Mod+H".action = focus-column-left;
+            "Mod+J".action = focus-window-down;
+            "Mod+K".action = focus-window-up;
+            "Mod+L".action = focus-column-right;
+            "Mod+Ctrl+H".action = move-column-left;
+            "Mod+Ctrl+J".action = move-window-down;
+            "Mod+Ctrl+K".action = move-window-up;
+            "Mod+Ctrl+L".action = move-column-right;
+            "Mod+Home".action = focus-column-first;
+            # "Mod+End ".action = focus-column-last;
+            "Mod+Ctrl+Home".action = move-column-to-first;
+            # "Mod+Ctrl+End ".action = move-column-to-last;
 
-              # Workspace switching
-              "Mod+U".action = focus-workspace-down;
-              "Mod+I".action = focus-workspace-up;
-              "Mod+Shift+U".action = move-workspace-down;
-              "Mod+Shift+I".action = move-workspace-up;
-              "Mod+Ctrl+U".action = move-column-to-workspace-down;
-              "Mod+Ctrl+I".action = move-column-to-workspace-up;
-              "Mod+1".action = focus-workspace 1;
-              "Mod+2".action = focus-workspace 2;
-              "Mod+3".action = focus-workspace 3;
-              "Mod+4".action = focus-workspace 4;
-              "Mod+5".action = focus-workspace 5;
-              "Mod+6".action = focus-workspace 6;
-              "Mod+7".action = focus-workspace 7;
-              "Mod+8".action = focus-workspace 8;
-              "Mod+9".action = focus-workspace 9;
+            # Workspace switching
+            "Mod+U".action = focus-workspace-down;
+            "Mod+I".action = focus-workspace-up;
+            "Mod+Shift+U".action = move-workspace-down;
+            "Mod+Shift+I".action = move-workspace-up;
+            "Mod+Ctrl+U".action = move-column-to-workspace-down;
+            "Mod+Ctrl+I".action = move-column-to-workspace-up;
+            "Mod+1".action = focus-workspace 1;
+            "Mod+2".action = focus-workspace 2;
+            "Mod+3".action = focus-workspace 3;
+            "Mod+4".action = focus-workspace 4;
+            "Mod+5".action = focus-workspace 5;
+            "Mod+6".action = focus-workspace 6;
+            "Mod+7".action = focus-workspace 7;
+            "Mod+8".action = focus-workspace 8;
+            "Mod+9".action = focus-workspace 9;
 
-              # Resize window
-              "Mod+Minus".action = set-column-width "-10%";
-              "Mod+Equal".action = set-column-width "+10%";
+            # Overview
+            "Mod+o".action = toggle-overview;
 
-              # Screenshot
-              # "Print".action = screenshot;
+            # Resize window
+            "Mod+Minus".action = set-column-width "-10%";
+            "Mod+Equal".action = set-column-width "+10%";
 
-              # Others
-              "Mod+Comma".action = consume-window-into-column;
-              "Mod+Period".action = expel-window-from-column;
-              "Mod+BracketLeft".action = consume-or-expel-window-left;
-              "Mod+BracketRight".action = consume-or-expel-window-right;
-              "Mod+Shift+P".action = power-off-monitors;
-              "Mod+R".action = switch-preset-column-width;
-              "Mod+Shift+R".action = reset-window-height;
-              "Mod+F".action = maximize-column;
-              "Mod+Shift+F".action = fullscreen-window;
-              "Mod+C".action = center-column;
-              "Mod+Ctrl+F".action = toggle-window-floating;
-              "Mod+V".action = toggle-column-tabbed-display;
+            # Screenshot
+            # "Print".action = screenshot;
 
-              "Mod+Shift+Q".action = quit;
-            };
+            # Others
+            "Mod+Comma".action = consume-window-into-column;
+            "Mod+Period".action = expel-window-from-column;
+            "Mod+BracketLeft".action = consume-or-expel-window-left;
+            "Mod+BracketRight".action = consume-or-expel-window-right;
+            "Mod+R".action = switch-preset-column-width;
+            "Mod+Shift+R".action = reset-window-height;
+            "Mod+F".action = maximize-column;
+            "Mod+Shift+F".action = fullscreen-window;
+            "Mod+C".action = center-column;
+            "Mod+Ctrl+F".action = toggle-window-floating;
+            "Mod+V".action = toggle-column-tabbed-display;
+
+            "Mod+Shift+Q".action = quit;
+            "Mod+Shift+Slash".action = show-hotkey-overlay;
+            "Mod+Shift+P".action = power-off-monitors;
+          };
 
           layout = {
             gaps = 10;
