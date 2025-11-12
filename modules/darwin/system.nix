e5806df7814c15a6f88ea611e21786c9c7599c4e
Luca Fondo <luca.fondo@trai.ch>
2025-11-12 17:29:07 +0100

    Additional macos system configuration

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,38 +1,50 @@
 { pkgs, ... }:
 {
-  system.defaults = {
-    dock = {
-      autohide = true;
-      autohide-delay = 0.0;
-      persistent-apps = [
-        "/System/Applications/Mail.app"
-        "/Applications/Arc.app"
-        "/System/Applications/System Settings.app"
-        "${pkgs.ghostty-bin}/Applications/Ghostty.app"
-        "/Applications/Spotify.app"
-      ];
-      expose-group-apps = true;
-      show-recents = false;
+  system = {
+    startup.chime = false;
+    defaults = {
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        persistent-apps = [
+          "/System/Applications/Mail.app"
+          "/Applications/Arc.app"
+          "/System/Applications/System Settings.app"
+          "${pkgs.ghostty-bin}/Applications/Ghostty.app"
+          "/Applications/Spotify.app"
+        ];
+        expose-group-apps = true;
+        show-recents = false;
+      };
+
+      finder.FXPreferredViewStyle = "Nlsv";
+
+      NSGlobalDomain = {
+        "com.apple.keyboard.fnState" = true;
+        AppleFontSmoothing = 0;
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = 1;
+        AppleShowAllExtensions = true;
+        KeyRepeat = 2;
+        AppleTemperatureUnit = "Celsius";
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSTableViewDefaultSizeMode = 1;
+      };
+
+      loginwindow.GuestEnabled = false;
+
+      CustomUserPreferences = {
+        "com.apple.symbolichotkeys" = {
+          AppleSymbolicHotKeys = {
+            "64" = {
+              enabled = false;
+            };
+          };
+        };
+      };
     };
-
-    finder.FXPreferredViewStyle = "Nlsv";
-
-    NSGlobalDomain = {
-      "com.apple.keyboard.fnState" = true;
-      AppleFontSmoothing = 0;
-      AppleICUForce24HourTime = true;
-      AppleInterfaceStyle = "Dark";
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = 1;
-      AppleShowAllExtensions = true;
-      KeyRepeat = 2;
-      AppleTemperatureUnit = "Celsius";
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSNavPanelExpandedStateForSaveMode = true;
-      NSTableViewDefaultSizeMode = 1;
-    };
-
-    loginwindow.GuestEnabled = false;
-
   };
 }
