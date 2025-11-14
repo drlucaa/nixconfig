5a3eb39e4832d81b448e0a8f76f256532f3562a2
Luca Fondo <luca.fondo@trai.ch>
2025-11-14 10:11:38 +0100

    Size up font in terminal

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -16,7 +16,7 @@
             if pkgs.stdenv.hostPlatform.isLinux then pkgs.unstable.ghostty else pkgs.unstable.ghostty-bin;
 
           settings = {
-            font-size = 12;
+            font-size = 16;
             font-family = "Monaspace Neon";
             mouse-hide-while-typing = true;
             shell-integration = "fish";
