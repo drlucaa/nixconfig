d01773043c270fae53c37457b5a5cd4543d9ad92
Luca Fondo <luca.fondo@trai.ch>
2025-11-14 15:38:03 +0100

    Add optional ableton, rekordbox, dropbox and jetbrains toolbox to homebrew

--- hosts/darwin/lucas-macbook/config.nix
+++ hosts/darwin/lucas-macbook/config.nix
@@ -1,1 +1,6 @@
-{...}:{}
+{ config, ... }:
+{
+  config.darwin.homebrew.ableton = true;
+  config.darwin.homebrew.rekordbox = true;
+  config.darwin.homebrew.dropbox = true;
+}
--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,33 +1,53 @@
-{ config, username, ... }:
+{ config, lib, ... }:
+let
+  cfg = config.darwin.homebrew;
+in
 {
-  homebrew = {
-    enable = true;
-    taps = builtins.attrNames config.nix-homebrew.taps;
-    brews = [
-      "block-goose-cli"
-      "borders"
-      "mas"
-      "docker"
-    ];
-    casks = [
-      "aerospace"
-      "1password"
-      "spotify"
-      "raycast"
-      "arc"
-      "linear-linear"
-    ];
-    masApps = {
-      "1Password for Safari" = 1569813296;
-    };
-    onActivation.cleanup = "zap";
-    onActivation.autoUpdate = true;
-    onActivation.upgrade = true;
+  options.darwin.homebrew = {
+    ableton = lib.mkEnableOption "Weather to install ableton";
+    rekordbox = lib.mkEnableOption "Weather to install ableton";
+    jetbrains-toolbox = lib.mkEnableOption "Weather to install jetbrains-toolbox";
+    dropbox = lib.mkEnableOption "Weather to install dropbox";
   };
 
-  home-manager.users.${username} =
-    { ... }:
-    {
-      xdg.configFile."aerospace/aerospace.toml".source = ../../confs/aerospace/aerospace.toml;
+  config = {
+    homebrew = {
+      enable = true;
+      taps = builtins.attrNames config.nix-homebrew.taps;
+      brews = [
+        "block-goose-cli"
+        "borders"
+        "mas"
+        "docker"
+      ];
+
+      casks = [
+        "aerospace"
+        "1password"
+        "spotify"
+        "raycast"
+        "arc"
+        "linear-linear"
+      ]
+      ++ lib.optionals cfg.ableton [
+        "ableton-live-standard"
+      ]
+      ++ lib.optionals cfg.rekordbox [
+        "rekordbox"
+      ]
+      ++ lib.optionals cfg.dropbox [
+        "dropbox"
+      ]
+      ++ lib.optionals cfg.jetbrains-toolbox [
+        "jetbrains-toolbox"
+      ];
+
+      masApps = {
+        "1Password for Safari" = 1569813296;
+      };
+      onActivation.cleanup = "zap";
+      onActivation.autoUpdate = true;
+      onActivation.upgrade = true;
     };
+  };
 }
