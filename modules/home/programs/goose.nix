09ab576734d028ae1ce364123fcce298f85467f6
Luca Fondo <luca.fondo@trai.ch>
2025-11-17 01:40:37 +0100

    Configure goose

--- modules/home/programs/goose.nix
+++ modules/home/programs/goose.nix
@@ -2,17 +2,38 @@
   inputs,
   username,
   lib,
-  pkgs,
   ...
 }:
 {
-  config = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+  config = {
+    environment.variables = {
+      RUST_LOG = "goose::providers::lead_worker=info";
+    };
+
     home-manager.users.${username} =
       { pkgs, ... }:
       {
-        home.packages = [
+        home.packages = lib.mkIf pkgs.stdenv.hostPlatform.isLinux [
           inputs.goose.defaultPackage.${pkgs.stdenv.hostPlatform.system}
         ];
+
+        xdg.configFile."goose/config.yaml".text = ''
+          GOOSE_PROVIDER: openrouter
+          GOOSE_LEAD_MODEL: anthropic/claude-sonnet-4.5
+          GOOSE_MODEL: qwen/qwen3-coder
+          GOOSE_CLI_SHOW_COST: true
+
+          extensions:
+            developer:
+              enabled: true
+              type: builtin
+              name: developer
+              description: Code editing and shell access
+              display_name: Developer Tools
+              timeout: 300
+              bundled: true
+              available_tools: []
+        '';
       };
   };
 }
