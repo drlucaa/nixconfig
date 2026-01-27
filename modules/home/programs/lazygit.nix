7710c552791870582c7ddaad71f7f5b3332732a9
Luca Fondo <luca.fondo@trai.ch>
2026-01-28 00:15:30 +0100

    Add gpg override to lazygit config

--- modules/home/programs/lazygit.nix
+++ modules/home/programs/lazygit.nix
@@ -10,6 +10,11 @@
         programs.lazygit = {
           enable = true;
           package = pkgs.unstable.lazygit;
+          settings = {
+            git = {
+              overrideGpg = true;
+            };
+          };
         };
 
         programs.fish.shellAbbrs = {
