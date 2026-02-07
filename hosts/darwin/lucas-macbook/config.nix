bf85e6ced3a363e598ef2c3ac8fab332741a2086
Luca Fondo <luca.fondo@trai.ch>
2026-02-07 04:13:19 +0100

    Add tailscale to macbook

--- hosts/darwin/lucas-macbook/config.nix
+++ hosts/darwin/lucas-macbook/config.nix
@@ -1,11 +1,14 @@
 { ... }:
 {
+  config.darwin.homebrew.extraBrews = [
+  ];
   config.darwin.homebrew.extraCasks = [
     "whatsapp"
     "ableton-live-standard"
     "ilok-license-manager"
     "rekordbox"
     "dropbox"
+    "tailscale-app"
   ];
 
 }
