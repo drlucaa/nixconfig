cd6f9c5f48a425665fd59cc75022063c99a301c1
Luca Fondo <luca.fondo@trai.ch>
2026-06-05 02:36:39 +0200

    Add myself to the trusted users

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -1,9 +1,19 @@
 {
+  username,
+  ...
+}:
+{
   determinateNix = {
     enable = true;
 
     customSettings = {
       download-buffer-size = 524000000;
+
+      trusted-users = [
+        "root"
+        "@admin"
+        username
+      ];
     };
   };
 }
