25c399bc37d9f89aea75c39e967b5cd7f6e22463
Luca Fondo <luca.fondo@trai.ch>
2026-07-20 20:10:11 +0200

    Remove adobe cc and replace dropbox with synology drive for lucas macbook

--- hosts/darwin/lucas-macbook/config.nix
+++ hosts/darwin/lucas-macbook/config.nix
@@ -3,12 +3,11 @@
   config.darwin.homebrew.extraBrews = [
   ];
   config.darwin.homebrew.extraCasks = [
-    "whatsapp"
     "ableton-live-suite"
-    "adobe-creative-cloud"
     "ilok-license-manager"
     "rekordbox"
-    "dropbox"
+    "synology-drive"
+    "whatsapp"
   ];
 
 }
